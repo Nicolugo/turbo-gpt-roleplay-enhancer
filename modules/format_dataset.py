8d15a8381ca6b3a388495e9580d9246ffd3bc9f5
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-04-28 11:55:06 +0000

    Remove temporary files

--- modules/format_dataset.py
+++ modules/format_dataset.py
@@ -1,4 +1,9 @@
 import os
 import json
 import glob
-from modules import 
+from modules import config
+
+def format():
+    json_dir = config.chats_folder
+    output_file = config.dataset_file
+    json_files = glob.glob(os.path.join(json_dir,
