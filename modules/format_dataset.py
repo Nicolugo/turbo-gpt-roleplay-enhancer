53ef4c2df807440edf0b60a8832a714cd519e136
Nicolugo <99984734+Nicolugo@users.noreply.github.com>
2023-04-28 18:17:04 +0000

    Ready update

--- modules/format_dataset.py
+++ modules/format_dataset.py
@@ -6,4 +6,10 @@ from modules import config
 def format():
     json_dir = config.chats_folder
     output_file = config.dataset_file
-    json_files = glob.glob(os.path.join(json_dir,
+    json_files = glob.glob(os.path.join(json_dir, '*.json'))
+
+    if len(json_files) < 10:
+        print("You need at least 10 chats for a fine-tune!")
+
+    with open(output_file, 'w') as outfile:
+        for jso
